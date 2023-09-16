3064c3c2fb5a90fa1c73d8a6ffd8550e24962a39
l4urab <you@example.com>
2023-09-16 21:02:43 +0200

    translation fixed

--- projekt-si-main/app/src/Form/Type/ContactType.php
+++ projekt-si-main/app/src/Form/Type/ContactType.php
@@ -9,9 +9,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Constraints\NotBlank; // Add this line
+
+// Add this line
 
 /**
  * Class ContactType.
@@ -74,7 +74,6 @@ class ContactType extends AbstractType
             ]
         );
     }
-
 
     /**
      * Returns the prefix of the template block name for this type.
